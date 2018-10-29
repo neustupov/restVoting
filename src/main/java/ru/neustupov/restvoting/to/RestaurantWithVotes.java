cb506e63fb645b7c396464d1a185032704d759f6
neustupov <neustupov@yandex.ru>
2018-10-29 22:26:42 +0300

    added @NoArgsConstructor annotation

--- src/main/java/ru/neustupov/restvoting/to/RestaurantWithVotes.java
+++ src/main/java/ru/neustupov/restvoting/to/RestaurantWithVotes.java
@@ -1,6 +1,7 @@
 package ru.neustupov.restvoting.to;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.neustupov.restvoting.model.AbstractNamedEntity;
 import ru.neustupov.restvoting.model.Menu;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
+@NoArgsConstructor
 @Entity
 @Table(name = "restaurants")
 public class RestaurantWithVotes extends AbstractNamedEntity {
